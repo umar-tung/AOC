dea52d7cc6e69981d9edd159f360efc6a47265ac
Umar Tung <umartung@Hope-Epondulans-MacBook-Pro.local>
2023-12-29 02:29:59 -0800

    Finished day 5 part 1

--- 2023/day5/day5-1.py
+++ 2023/day5/day5-1.py
@@ -1,6 +1,3 @@
-from io import TextIOWrapper
-
-
 def parse_seeds(raw_str: str) -> list[int]:
     return [int(x) for x in raw_str.split(": ")[1].split()]
 
@@ -14,15 +11,20 @@ TEMPERATURE_TO_HUMIDITY = "temperature-to-humidity"
 HUMIDITY_TO_LOCATION = "humidity-to-location"
 
 
-def search_maps(mappings: dict[str, list[int]], start_seed: int) -> int:
+def search_maps(mappings: dict[str, list[list[int]]], start_seed: int) -> int:
     cur_val = start_seed
-    for mapping in mappings.values():
-        cur_val = mapping[cur_val] if cur_val in mapping else cur_val
+    for ranges in mappings.values():
+        for dst, src, rnge in ranges:
+            if cur_val >= int(src) and cur_val < int(src) + int(rnge):
+                cur_val = int(dst) + cur_val - int(src)
+                break
+        print(f"{cur_val}-> ", end="")
+    print(f"fin: {cur_val}")
     return cur_val
 
 
 with open("data.txt") as file:
-    mappings: dict[str, list[int]] = {
+    mappings: dict[str, list[list[int]]] = {
         SEED_TO_SOIL: [],
         SOIL_TO_FERTALIZER: [],
         FERTALIZER_TO_WATER: [],
@@ -35,21 +37,19 @@ with open("data.txt") as file:
     raw_seed_data = file.readline()
 
     seeds = parse_seeds(raw_seed_data)
+    print(seeds)
 
+    cur_line = file.readline().strip()
     for mapping in mappings:
-        cur_line = file.readline().strip()
-        while cur_line and mapping not in cur_line:
+        while not cur_line or mapping in cur_line:
             cur_line = file.readline().strip()
-
-        cur_line = file.readline().strip()
-        cur_line = file.readline().strip()
-
         while cur_line:
             dst, src, rnge = [int(x) for x in cur_line.split()]
 
-            mappings[mapping] = [dst, src, rnge]
+            mappings[mapping].append([dst, src, rnge])
 
             cur_line = file.readline().strip()
+        print(f"{mapping}: {mappings[mapping]}")
 
     min_loc = min([search_maps(mappings, seed) for seed in seeds])
     print(min_loc)

ec111f41ef6f0455bd43ca61b9d32a0204bdd618
Umar Tung <umartung@gmail.com>
2025-11-10 20:44:22 -0800

    update day2

--- 2024/day2/safereports.py
+++ 2024/day2/safereports.py
@@ -5,7 +5,7 @@ filename = "test.txt"
 
 def comparison(nums, func):
     for i in range(1, len(nums)):
-        if func(nums[i-1], nums[i]):
+        if not func(nums[i-1], nums[i]):
             return False
     return True
 
@@ -14,8 +14,8 @@ with open(filename) as file:
     for line in file:
         parsed_line = [int(x) for x in line.strip().split(" ")]
 
-        if (comparison(parsed_line, lambda x, y : x < y) or comparison(parsed_line, lambda x, y: x > y)) and \
-            comparison(parsed_line, lambda x, y : abs(x - y) > 3 or abs(x - y) < 1):
+        if (comparison(parsed_line, lambda x, y : x < y) or comparison(parsed_line, lambda x, y: x > y)) and\
+            comparison(parsed_line, lambda x, y : abs(x - y) <= 3 and abs(x - y) >= 1):
             score +=1
 
     print(score)
